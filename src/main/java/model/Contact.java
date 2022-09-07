b9462bf8b60da1f3b7c88b77278f04355b19cbfe
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-07 09:48:38 +0200

    Delete constructor, toString()

--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -5,11 +5,6 @@ public abstract class Contact {
     private String name;
     private String telefonnummer;
 
-    public Contact(String name, String telefonnummer){
-        this.name = name;
-        this.telefonnummer = telefonnummer;
-    }
-
     public Contact() {
 
     }
@@ -21,11 +16,4 @@ public abstract class Contact {
         return telefonnummer;
     }
 
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", telefonnummer='" + telefonnummer + '\'' +
-                '}';
-    }
 }
